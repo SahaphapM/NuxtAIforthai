accac88f23626ed3a2c21077799841da0c1f1738
SahaphapM <65160309@go.buu.ac.th>
2024-10-31 17:51:56 +0700

    apiAiThaiTest

---
+++ pages/apiCap/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="file" @change="fileStore.handleFileChange" />
+    <button @click="fileStore.submitFileCap">Submit File Cap</button>
+    <div><img v-if="fileStore.file" :src="fileStore.fileUrl" /></div>
+
+    <div v-if="fileStore.caption">{{ fileStore.caption }}</div>
+  </div>
+  <div>
+    <input type="file" @change="fileStore.handleFileChange" />
+    <button @click="fileStore.submitFileSupResolution">
+      Submit File Super Resolution
+    </button>
+    <div><img v-if="fileStore.file" :src="fileStore.fileUrl" /></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const fileStore = useFileStore();
+computed(() => (fileStore.file ? URL.createObjectURL(fileStore.file) : null));
+</script>
